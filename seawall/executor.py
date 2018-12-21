b9063480287fab0fcbc39965eb7a34cad85c8e55
Dan Leehr <dan.leehr@duke.edu>
2018-12-20 22:09:26 -0500

    Comment about max ram and cores

--- seawall/executor.py
+++ seawall/executor.py
@@ -6,6 +6,9 @@ log = logging.getLogger("seawall.executor")
 
 class SeawallExecutor(MultithreadedJobExecutor):
 
+    # MultithreadedJobExecutor sets self.max_ram and self.max_cores to the local machine's resources
+    # TODO: override init to set this
+
     def run_jobs(self,
                  process,           # type: Process
                  job_order_object,  # type: Dict[Text, Any]
