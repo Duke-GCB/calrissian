335ee7311e89fe4104e32a09a71940211087cc54
Dan Leehr <dan.leehr@duke.edu>
2019-01-11 14:47:33 -0500

    Add README, remove TODOs
    TODOs have been created as issues

--- calrissian/executor.py
+++ calrissian/executor.py
@@ -7,7 +7,6 @@ log = logging.getLogger("calrissian.executor")
 class CalrissianExecutor(MultithreadedJobExecutor):
 
     # MultithreadedJobExecutor sets self.max_ram and self.max_cores to the local machine's resources
-    # TODO: override init to set this
 
     def run_jobs(self,
                  process,           # type: Process
