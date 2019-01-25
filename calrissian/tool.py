3704bc53cf1015c0d139d124cba00489d20637df
Dan Leehr <dan.leehr@duke.edu>
2019-01-25 11:59:14 -0500

    Restore injection of DockerRequirement at tool level
    Had switched to determining the container inside the job, but when I'm testing for the first time running CalrissianJob-revsort-single-default-container.yaml, it fails with a permission denied error.
    Testing if the DockerRequirement needs to be present before the job is called

--- calrissian/tool.py
+++ calrissian/tool.py
@@ -19,8 +19,20 @@ class CalrissianCommandLineTool(CommandLineTool):
         :return: a callable that runs the job
         """
         # This implementation runs CommandLineTools exclusively in containers
+        # so we need to add a DockerRequirement if it's not present
         if not runtimeContext.use_container:
             raise CalrissianToolException('Unable to create a CalrissianCommandLineTool - use_container is disabled')
+        docker_requirement, _ = self.get_requirement('DockerRequirement')
+        if not docker_requirement:
+            # no docker requirement specified, inject one
+            default_container = runtimeContext.find_default_container(self)
+            if not default_container:
+                raise CalrissianToolException('Unable to create a CalrissianCommandLineTool - '
+                                              'tool has no DockerRequirement and no default_container specified')
+            self.requirements.insert(0, {
+                'class': 'DockerRequirement',
+                'dockerPull': default_container
+            })
         return CalrissianCommandLineJob
 
 
