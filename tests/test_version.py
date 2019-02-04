06d72d4701f8647809c540bcf52e7e816381749b
Dan Leehr <dan.leehr@duke.edu>
2019-02-04 11:13:38 -0500

    Update version tests for package lookup

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,27 @@
 from unittest import TestCase
-from calrissian.version import version
+from unittest.mock import Mock, patch
+
+from calrissian.version import version, package_version
 
 
 class VersionTestCase(TestCase):
 
-    def test_version(self):
-        self.assertEqual(version(), 'Calrissian-dev')
+    @patch('calrissian.version.cwltool_version')
+    @patch('calrissian.version.calrissian_version')
+    def test_assembles_vession_string(self, mock_calrissian_version, mock_cwltool_version):
+        mock_cwltool_version.return_value = '3.2.1'
+        mock_calrissian_version.return_value = '1.2.3'
+        self.assertEqual(version(), 'calrissian 1.2.3 (cwltool 3.2.1)')
 
+    @patch('calrissian.version.pkg_resources')
+    def test_package_version_unknown(self, mock_pkg_resources):
+        mock_pkg_resources.DistributionNotFound = Exception
+        mock_pkg_resources.require.side_effect = mock_pkg_resources.DistributionNotFound()
+        self.assertEqual(package_version('package-name'), 'unknown')
+        self.assertTrue(mock_pkg_resources.require.called)
+
+    @patch('calrissian.version.pkg_resources')
+    def test_package_version_known(self, mock_pkg_resources):
+        mock_pkg_resources.require.return_value = [Mock(version='1.2.3'), ]
+        self.assertEqual(package_version('package-name'), '1.2.3')
+        self.assertTrue(mock_pkg_resources.require.called)
