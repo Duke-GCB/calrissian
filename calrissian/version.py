778dffbda99524dd99c95e021adba2a36bb2df40
Dan Leehr <dan.leehr@duke.edu>
2019-02-04 10:46:18 -0500

    Update version reporting
    - Uses package version of calrissian and cwltool

--- calrissian/version.py
+++ calrissian/version.py
@@ -1,3 +1,21 @@
+import pkg_resources  # part of setuptools
+
+
+def package_version(package_name):
+    pkg = pkg_resources.require(package_name)
+    if pkg:
+        return pkg[0].version
+    else:
+        return 'unknown'
+
+
+def cwltool_version():
+    return package_version('cwltool')
+
+
+def calrissian_version():
+    return package_version('calrissian')
+
 
 def version():
-    return 'Calrissian-dev'
+    return 'calrissian {} (cwltool {})'.format(calrissian_version(), cwltool_version())
