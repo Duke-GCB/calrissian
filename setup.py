f45a4bbb13fcc5864e2a5d740f168c02533dda41
Dan Leehr <dan.leehr@duke.edu>
2019-02-06 14:15:35 -0500

    Adds CircleCI config to upload to PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
+import os
+import sys
 from setuptools import setup
+# version checking derived from https://github.com/levlaz/circleci.py/blob/master/setup.py
+from setuptools.command.install import install
+
+VERSION = '0.1.0'
+TAG_ENV_VAR = 'CIRCLE_TAG'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv(TAG_ENV_VAR)
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
 setup(
     name='calrissian',
-    version='0.1.0',
+    version=VERSION,
     author='Dan Leehr',
     author_email='dan.leehr@duke.edu',
     description='CWL runner for Kubernetes',
@@ -34,4 +56,7 @@ setup(
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: System :: Distributed Computing',
     ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
 )
