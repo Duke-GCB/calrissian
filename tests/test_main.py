22f8ad9f152828286fc16ca0ff069d83d6a21980
Dan Leehr <dan.leehr@duke.edu>
2019-02-08 16:14:26 -0500

    Implement signal handler tests for main, reformat tests_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch, call, Mock
-from calrissian.main import main, add_arguments, parse_arguments
+
+from calrissian.main import main, add_arguments, parse_arguments, handle_sigterm, install_signal_handler
 
 
 class CalrissianMainTestCase(TestCase):
@@ -14,22 +15,29 @@ class CalrissianMainTestCase(TestCase):
     @patch('calrissian.main.parse_arguments')
     @patch('calrissian.main.add_arguments')
     @patch('calrissian.main.delete_pods')
-    def test_main_calls_cwlmain_returns_exit_code(self, mock_delete_pods, mock_add_arguments, mock_parse_arguments, mock_version, mock_runtime_context, mock_loading_context, mock_executor, mock_arg_parser, mock_cwlmain):
+    @patch('calrissian.main.install_signal_handler')
+    def test_main_calls_cwlmain_returns_exit_code(self, mock_install_signal_handler, mock_delete_pods,
+                                                  mock_add_arguments, mock_parse_arguments, mock_version,
+                                                  mock_runtime_context, mock_loading_context, mock_executor,
+                                                  mock_arg_parser, mock_cwlmain):
         mock_exit_code = Mock()
-        mock_cwlmain.return_value = mock_exit_code
+        mock_cwlmain.return_value = mock_exit_code  # not called before main
         result = main()
         self.assertTrue(mock_arg_parser.called)
         self.assertEqual(mock_add_arguments.call_args, call(mock_arg_parser.return_value))
         self.assertEqual(mock_parse_arguments.call_args, call(mock_arg_parser.return_value))
-        self.assertEqual(mock_executor.call_args, call(mock_parse_arguments.return_value.max_ram, mock_parse_arguments.return_value.max_cores))
+        self.assertEqual(mock_executor.call_args,
+                         call(mock_parse_arguments.return_value.max_ram, mock_parse_arguments.return_value.max_cores))
         self.assertTrue(mock_runtime_context.called)
         self.assertEqual(mock_cwlmain.call_args, call(args=mock_parse_arguments.return_value,
                                                       executor=mock_executor.return_value,
                                                       loadingContext=mock_loading_context.return_value,
                                                       runtimeContext=mock_runtime_context.return_value,
                                                       versionfunc=mock_version))
-        self.assertEqual(mock_runtime_context.return_value.select_resources, mock_executor.return_value.select_resources)
+        self.assertEqual(mock_runtime_context.return_value.select_resources,
+                         mock_executor.return_value.select_resources)
         self.assertEqual(result, mock_exit_code)
+        self.assertTrue(mock_delete_pods.called)  # called after main()
 
     def test_add_arguments(self):
         mock_parser = Mock()
@@ -67,3 +75,18 @@ class CalrissianMainTestCase(TestCase):
         self.assertEqual(parsed, mock_parser.parse_args.return_value)
         self.assertTrue(mock_print_version.called)
         self.assertEqual(mock_sys.exit.call_args, call(0))
+
+    @patch('calrissian.main.sys')
+    @patch('calrissian.main.delete_pods')
+    def test_handle_sigterm_exits_with_signal(self, mock_delete_pods, mock_sys):
+        frame = Mock()
+        signum = 15
+        handle_sigterm(signum, frame)
+        self.assertEqual(mock_sys.exit.call_args, call(signum))
+        self.assertTrue(mock_delete_pods.called)
+
+    @patch('calrissian.main.signal')
+    @patch('calrissian.main.handle_sigterm')
+    def test_install_signal_handler(self, mock_handle_sigterm, mock_signal):
+        install_signal_handler()
+        self.assertEqual(mock_signal.signal.call_args, call(mock_signal.SIGTERM, mock_handle_sigterm))
