da1996e447501833502aef2b91b8867d34adf12b
Dan Leehr <dan.leehr@duke.edu>
2019-02-28 14:16:39 -0500

    Adds tests for events/processors and timeline duration

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from calrissian.report import TimedReport, TimedResourceReport, TimelineReport
+from calrissian.report import Event, MaxParallelCountProcessor, MaxParallelCPUsProcessor, MaxParallelRAMProcessor
 from freezegun import freeze_time
+from unittest.mock import Mock, patch, call
 import datetime
 
 TIME_1000 = datetime.datetime(2000, 1, 1, 10, 0, 0)
@@ -45,7 +47,7 @@ class TimedReportTestCase(TestCase):
         self.report.finish_time = TIME_1100
         self.assertEqual(1.0, self.report.elapsed_hours())
 
-    def test_elapsed_fails_if_not_started(self):
+    def test_elapsed_raises_if_not_started(self):
         self.assertEqual(self.report.start_time, None)
         with self.assertRaises(TypeError):
             self.report.elapsed_seconds()
@@ -110,6 +112,24 @@ class TimelineReportTestCase(TestCase):
         self.report.add_report(TimedResourceReport())
         self.report.add_report(TimedResourceReport())
         self.assertEqual(self.report.total_tasks(), 2)
+
+    def test_calculates_start_finish_times(self):
+        self.assertIsNone(self.report.start_time)
+        self.assertIsNone(self.report.finish())
+        self.report.add_report(TimedResourceReport(start_time=TIME_1015, finish_time=TIME_1100))
+        self.report.add_report(TimedResourceReport(start_time=TIME_1000, finish_time=TIME_1030))
+        self.report.add_report(TimedResourceReport(start_time=TIME_1030, finish_time=TIME_1045))
+        self.assertEqual(self.report.start_time, TIME_1000)
+        self.assertEqual(self.report.finish_time, TIME_1100)
+
+    def test_calculates_duration(self):
+        self.report.add_report(TimedResourceReport(start_time=TIME_1000, finish_time=TIME_1015))
+        self.report.add_report(TimedResourceReport(start_time=TIME_1045, finish_time=TIME_1100))
+        self.assertEqual(self.report.elapsed_hours(), 1.0)
+
+    def test_elapsed_raises_with_no_tasks(self):
+        with self.assertRaises(TypeError):
+            self.report.elapsed_seconds()
 
     def test_max_parallel_tasks(self):
         # Count task parallelism. 3 total tasks, but only 2 at a given time
@@ -176,16 +196,98 @@ class TimelineReportTestCase(TestCase):
 
 
 class EventTestCase(TestCase):
-    pass
 
-class EventProcessorTestCase(TestCase):
-    pass
+    def test_init(self):
+        mock_report = Mock()
+        event = Event(TIME_1100, Event.START, mock_report)
+        self.assertEqual(event.time, TIME_1100)
+        self.assertEqual(event.type, Event.START)
+        self.assertEqual(event.report, mock_report)
+
+    def test_start_event_classmethod(self):
+        mock_report = Mock(start_time=TIME_1000)
+        event = Event.start_event(mock_report)
+        self.assertEqual(event.time, TIME_1000)
+        self.assertEqual(event.type, Event.START)
+        self.assertEqual(event.report, mock_report)
+
+    def test_finish_event_classmethod(self):
+        mock_report = Mock(finish_time=TIME_1100)
+        event = Event.finish_event(mock_report)
+        self.assertEqual(event.time, TIME_1100)
+        self.assertEqual(event.type, Event.FINISH)
+        self.assertEqual(event.report, mock_report)
+
+    def test_processor(self):
+        mock_report = Mock()
+        event = Event(TIME_1100, Event.START, mock_report)
+        mock_processor = Mock()
+        event.process(mock_processor)
+        self.assertTrue(mock_processor.process.call_args, call(mock_report, Event.START))
+
 
 class MaxParallelCountProcessorTestCase(TestCase):
-    pass
+
+    def test_init(self):
+        processor = MaxParallelCountProcessor()
+        self.assertEqual(processor.count, 0)
+        self.assertEqual(processor.max, 0)
+
+    def test_count_unit(self):
+        processor = MaxParallelCountProcessor()
+        self.assertEqual(processor.count_unit(Mock()), 1)
+
+    def test_process_start_event(self):
+        processor = MaxParallelCountProcessor()
+        mock_report = Mock()
+        self.assertEqual(processor.count, 0)
+        processor.process(mock_report, Event.START)
+        self.assertEqual(processor.count, 1)
+        self.assertEqual(processor.max, 1)
+
+    def test_process_finish_event(self):
+        processor = MaxParallelCountProcessor()
+        processor.count = 1 # set to 1 since finish should step down
+        processor.max = 1
+        mock_report = Mock()
+        self.assertEqual(processor.count, 1)
+        processor.process(mock_report, Event.FINISH)
+        self.assertEqual(processor.count, 0)
+        self.assertEqual(processor.max, 1) # Max should not drop down
+
+    def test_process_simple_event_stream(self):
+        processor = MaxParallelCountProcessor()
+        mock_report = Mock()
+        processor.process(mock_report, Event.START)
+        processor.process(mock_report, Event.FINISH)
+        self.assertEqual(processor.result(), 1)
+
+    def test_process_complicated_event_stream(self):
+        # get the max up to 3
+        processor = MaxParallelCountProcessor()
+        mock_report = Mock()
+        processor.process(mock_report, Event.START)
+        processor.process(mock_report, Event.START)
+        processor.process(mock_report, Event.START)
+        processor.process(mock_report, Event.FINISH)
+        processor.process(mock_report, Event.START)
+        processor.process(mock_report, Event.FINISH)
+        processor.process(mock_report, Event.FINISH)
+        processor.process(mock_report, Event.FINISH)
+        self.assertEqual(processor.result(), 3)
+
 
 class MaxParallelCPUsProcessorTestCase(TestCase):
-    pass
+
+    def test_count_unit(self):
+        processor = MaxParallelCPUsProcessor()
+        mock_report = Mock(cpus=10)
+        self.assertEqual(processor.count_unit(mock_report), 10)
+
 
 class MaxParallelRAMProcessorTestCase(TestCase):
-    pass
+
+    def test_count_unit(self):
+        processor = MaxParallelRAMProcessor()
+        mock_report = Mock(ram_megabytes=512)
+        self.assertEqual(processor.count_unit(mock_report), 512)
