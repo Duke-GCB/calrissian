9977cba1fa742702be4c35c70fdb549a83a40a31
Dan Leehr <dan.leehr@duke.edu>
2019-03-01 15:10:59 -0500

    Update usage-report argument to indicate JSON

--- calrissian/main.py
+++ calrissian/main.py
@@ -23,7 +23,7 @@ def add_arguments(parser):
     parser.add_argument('--max-ram', type=int, help='Maximum amount of RAM in MB to use')
     parser.add_argument('--max-cores', type=int, help='Maximum number of CPU cores to use')
     parser.add_argument('--pod-labels', type=Text, nargs='?', help='YAML file of labels to add to Pods submitted')
-    parser.add_argument('--usage-report', type=Text, nargs='?', help='Output YAML file name to record resource usage')
+    parser.add_argument('--usage-report', type=Text, nargs='?', help='Output JSON file name to record resource usage')
 
 
 def print_version():
