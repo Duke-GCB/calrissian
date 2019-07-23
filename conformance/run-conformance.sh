85fb0b0b73e844e52f94e6312ccc12c01e830053
Dan Leehr <dan.leehr@duke.edu>
2019-07-23 15:41:25 -0400

    Script to run conformance tests attached

---
+++ conformance/run-conformance.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+kubectl run -i --rm --tty conformance-tests --overrides='
+{
+  "spec": {
+    "containers": [
+      {
+        "name": "conformance-tests",
+        "image": "calrissian:conformance",
+        "workingDir": "/conformance/common-workflow-language-1.0.2",
+        "command": ["./run_test.sh"],
+        "args": ["RUNNER=calrissian", "--verbose", "EXTRA=--max-ram 8G --max-cores 4 --default-container debian:stretch-slim"],
+        "stdin": true,
+        "stdinOnce": true,
+        "tty": true,
+        "env": [
+          { "name":"CALRISSIAN_POD_NAME", "valueFrom": { "fieldRef": { "fieldPath": "metadata.name" } } },
+          { "name": "TMPDIR", "value": "/output" }
+        ],
+        "volumeMounts": [
+          { "mountPath": "/conformance", "name": "conformance-test-data", "readOnly": true },
+          { "mountPath": "/output", "name": "conformance-output-data" }
+        ]
+      }
+    ],
+    "restartPolicy": "Never",
+    "volumes": [
+    { "name": "conformance-test-data", "persistentVolumeClaim": {"claimName":"conformance-test-data", "readOnly": true } },
+    { "name": "conformance-output-data", "persistentVolmeClaim": {"claimName": "conformance-output-data" } }
+    ]
+  }
+}
+'  --image=calrissian:conformance --restart=Never
