9dd5e258ecea0bdd7d14a5add651ef9d308ee42e
Dan Leehr <dan.leehr@duke.edu>
2019-09-06 10:46:42 -0400

    Remove thread debugging code

--- calrissian/thread_pool_executor.py
+++ calrissian/thread_pool_executor.py
@@ -14,28 +14,6 @@ class JobAlreadyExistsException(Exception):
     pass
 
 
-def tname(msg):
-    log.debug('{} {}'.format(msg, threading.current_thread()))
-
-
-def raise_if_not_main():
-    if threading.current_thread() != threading.main_thread():
-        raise Exception('this should only be called from main thread')
-
-
-def raise_if_main():
-    if threading.current_thread() == threading.main_thread():
-        raise Exception('this should not be called from main thread')
-
-
-def report_state(future):
-    if future.done():
-        log.info('done')
-    if future.cancelled():
-        log.info('cancelled')
-    if future.running():
-        log.info('running')
-
 class Resources(object):
 
     RAM = 'ram'
@@ -92,13 +70,11 @@ class JobResourceQueue(object):
     """
 
     def __init__(self, priority=Resources.RAM, descending=False):
-        raise_if_not_main()
         self.jobs = dict()
         self.priority = priority
         self.descending = descending
 
     def add(self, job):
-        raise_if_not_main()
         """
         Add the job and extract its resources
         :param job:
@@ -110,14 +86,12 @@ class JobResourceQueue(object):
             self.jobs[job] = Resources.from_job(job)
 
     def is_empty(self):
-        raise_if_not_main()
         return len(self.jobs) == 0
 
     def sorted_jobs(self):
         return sorted(self.jobs.items(), key=lambda item: getattr(item[1], self.priority), reverse=self.descending)
 
     def pop_runnable_jobs(self, resource_limit):
-        raise_if_not_main()
         """
         Collect a dictionary of jobs and resources within the specified limit and return them.
         May return an empty dictionary if nothing fits in the resource limit
@@ -155,7 +129,6 @@ class ThreadPoolJobExecutor(JobExecutor):
             return self.total_resources - self.allocated_resources
 
     def run_job(self, job, runtime_context):
-        raise_if_main()
         log.debug('run job {}'.format(job))
         if job:
             job.run(runtime_context)
@@ -164,8 +137,6 @@ class ThreadPoolJobExecutor(JobExecutor):
         # Note that job callbacks are invoked on a thread belonging to the process that added them
         # So it's not the main thread. If the callback itself raises an Exception, that Exception is logged and ignored
         # If we want to stop executing because of an exception we do have some other cleanup to do
-        tname('job callback')
-        report_state(future)
         # The done_callback is invoked on a background thread.
         self.restore(rsc) # Always restore the resources
 
@@ -186,13 +157,10 @@ class ThreadPoolJobExecutor(JobExecutor):
         :param pending_futures: A set() of futures that should be cancelled if an exception occurred
         :return: None
         """
-        raise_if_not_main()
         # Code translated from SingleJobExecutor.run_jobs
         # It raises WorkflowExceptions and ValidationException directly.
         # Other Exceptions are converted to WorkflowException
-        tname('raise_exceptions')
         if not self.exceptions.empty():
-            tname('have exceptions')
             # There's at least one exception, cancel all pending jobs
             # Note that cancel will only matter if there aren't enough available threads to start processing the job in
             # the first place. Once the function starts running it cannot be cancelled.
@@ -215,7 +183,6 @@ class ThreadPoolJobExecutor(JobExecutor):
         :return:
         """
         rsc = Resources.from_job(job)
-        tname('raise if too big {} {}'.format(rsc, self.total_resources))
         if not rsc <= self.total_resources:
             raise WorkflowException('Job {} requires resources {} that exceed total resources {}'.
                                     format(job, rsc, self.total_resources))
@@ -236,7 +203,6 @@ class ThreadPoolJobExecutor(JobExecutor):
         runs those jobs using the pool executor, and returns
         :return: None
         """
-        raise_if_not_main()
         log.info('processing queue')
         runnable_jobs = self.jrq.pop_runnable_jobs(self.available_resources) # Removes jobs from the queue
         if raise_on_unavalable and not runnable_jobs and not self.jrq.is_empty():
@@ -262,7 +228,6 @@ class ThreadPoolJobExecutor(JobExecutor):
         self.handle_queued_exceptions(wait_results.not_done)
 
     def run_jobs(self, process, job_order_object, logger, runtime_context):
-        raise_if_not_main()
         if runtime_context.workflow_eval_lock is None:
             raise WorkflowException("runtimeContext.workflow_eval_lock must not be None")
         log.info('initial available {}'.format(self.available_resources))
