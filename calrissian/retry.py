879b74477d519784c27c21f42c91deb4b1fe2747
Dan Leehr <dan.leehr@duke.edu>
2019-09-26 11:30:00 -0400

    WIP on retry, now logging too and stopping

--- calrissian/retry.py
+++ calrissian/retry.py
@@ -1,11 +1,15 @@
-from tenacity import retry, wait, wait_exponential, retry_if_exception_type
+from tenacity import retry, wait, wait_exponential, retry_if_exception_type, stop_after_attempt, before_sleep_log
+import logging
 import os
-import functools
+
+
+logger = logging.getLogger('calrissian.retry')
 
 class WaitRetry(object):
-    MULTIPLIER = int(os.getenv('WAIT_RETRY_MULTIPLIER', 1))
-    MIN = int(os.getenv('WAIT_RETRY_MIN', 4))
-    MAX = int(os.getenv('WAIT_RETRY_MAX', 10))
+    MULTIPLIER = int(os.getenv('WAIT_RETRY_MULTIPLIER', 1)) # time unit for waiting between retries, e.g. 1 second
+    MIN = int(os.getenv('WAIT_RETRY_MIN', 10)) # starting interval for retrying
+    MAX = int(os.getenv('WAIT_RETRY_MAX', 60)) # max interval between retries
+    STOP = int(os.getenv('WAIT_ATTEMPTS', 3)) # Max number of retries before giving up
 
 
 # types can be a tuple
@@ -13,7 +17,9 @@ def retry_exponential_if_exception_type(exc_class):
     def decorator_retry(func):
         @retry(
             retry=retry_if_exception_type(exc_class),
-            wait=wait_exponential(multiplier=WaitRetry.MULTIPLIER, min=WaitRetry.MIN, max=WaitRetry.MAX)
+            wait=wait_exponential(multiplier=WaitRetry.MULTIPLIER, min=WaitRetry.MIN, max=WaitRetry.MAX),
+            stop=stop_after_attempt(WaitRetry.STOP),
+            before_sleep=before_sleep_log(logger, logging.DEBUG)
         )
         def wrapper(*args, **kwargs):
             func(*args, **kwargs)
