c933931618d6663d20bfa63f39f95433af001817
Fabrice Brito <fabrice.brito@terradue.com>
2023-11-23 14:35:35 +0100

    conformance results

--- conformance/run-job-1-2.sh
+++ conformance/run-job-1-2.sh
@@ -6,4 +6,5 @@ NAMESPACE_NAME="calrissian-conformance"
 
 kubectl create --namespace="$NAMESPACE_NAME" -f ConformanceTestsJob-1.2.yaml
 kubectl wait --namespace="$NAMESPACE_NAME" --for=condition=Ready --selector=job-name=conformance-tests-1-2 pods
-kubectl logs --namespace="$NAMESPACE_NAME" -f jobs/conformance-tests-1-2 > results-1-2.txt
+kubectl logs --namespace="$NAMESPACE_NAME" -f jobs/conformance-tests-1-2 | egrep -v 'The `label`' > results-v1.2.txt
+kubectl cp $NAMESPACE_NAME/inspect-volumes:/output/badges-1.2.0/ badges/1.2.0
