109e6e1c98f600a7e6cc3ed7bbdceb593b312ffa
Fabrice Brito <fabrice.brito@terradue.com>
2025-09-04 11:23:49 +0200

    fixes clusterMaxCore -> clusterMaxCores

--- tests/test_tool.py
+++ tests/test_tool.py
@@ -77,7 +77,7 @@ class CalrissianCommandLineToolTestCase(TestCase):
                 "workerCores": 2,
                 "workerCoresLimit": 2,
                 "workerMemory": "4G",
-                "clusterMaxCore": 8,
+                "clusterMaxCores": 8,
                 "clusterMaxMemory": "16G",
                 "class": "https://calrissian-cwl.github.io/schema#DaskGatewayRequirement"  # From cwl
             }
